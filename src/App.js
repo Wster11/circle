87c0d77e8f7abac8fa2f73079fcf7d4960c0ef2e
wangying <wangying@wangyingdeMacBook-Pro.local>
2022-06-22 16:20:57 +0800

    feat: login

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import './App.less';
 import { Routes, Route, Navigate} from "react-router-dom";
 import Login from './layout/login/login'
 import Main from './layout/main/main'
--- src/layout/login/login.js
+++ src/layout/login/login.js
@@ -1,18 +1,45 @@
-
-import './login.less';
+import s from './login.module.less';
 import { useNavigate } from 'react-router-dom'
-import { Button } from 'antd';
+import { Input, Button } from 'antd';
+
+export const EyeIcon = ()=>{
+    return (
+        <div className='eye-icon'></div>
+    )
+}
+export const EyeSlashIcon = ()=>{
+    return (
+        <div className='eye-slash'></div>
+    )
+}
 
 function Login() {
     let navigate = useNavigate();
     const login = () => {
         navigate("/main")
     }
+    const onChange = (e) => {
+        console.log(e);
+    };
+    const LOGIN = {
+        loginText: '登录环信超级社区',
+        usernamePlaceHolder: '请输入您的手机号',
+        passwordPlaceHolder: '请输入密码',
+        login: '登录',
+    };
     return (
-        <div className="login" onClick={login}>
-            login
-            <span className='username'>用户名：<span className='aa'>username</span></span>
-            <Button type="primary">Button</Button>
+        <div className="login-container">
+            <div className='login-layout'>
+                <div className={s.loginIcon}></div>
+                <div className='login-text'>{LOGIN.loginText}</div>
+                <div className='login-main'>
+                    <Input className='username-input' placeholder={LOGIN.usernamePlaceHolder} allowClear={{ clearIcon: <div className='clearIcon'></div> }} maxLength={11} onChange={onChange} />
+                    <Input.Password className='password-input' placeholder={LOGIN.passwordPlaceHolder} iconRender={(visible) => (visible ? <EyeIcon/>: <EyeSlashIcon/>)} />
+                    <div className='login-button login-disable'>{LOGIN.login}</div>
+                </div>
+            </div>
+
+            {/* <Button type="primary" onClick={login}>Button</Button> */}
         </div>
     );
 }
